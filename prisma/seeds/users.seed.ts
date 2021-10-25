1d67f8bf99c88bda70bf119a2d393bee8016f609
Patrick Lazo <patrickdz96@gmail.com>
2021-10-25 16:45:36 -0500

    upgrade npm and configuration seed

---
+++ prisma/seeds/users.seed.ts
@@ -0,0 +1,29 @@
+import { Prisma } from '@prisma/client';
+import { prisma } from '../seed';
+import * as bcrypt from 'bcrypt';
+
+const users: Prisma.UserCreateInput[] = [
+  {
+    email: 'patricklazo@ravn.co',
+    firstName: 'Patrick',
+    lastName: 'Lazo',
+    password: '123456',
+  },
+  {
+    email: 'alexander@ravn.co',
+    firstName: 'Alexander',
+    lastName: 'Pinto',
+    password: '123456',
+  },
+];
+
+const usersSeed = async () => {
+  for (const user of users) {
+    user.password = await bcrypt.hash(user.password, 10);
+    await prisma.user.create({
+      data: user,
+    });
+  }
+};
+
+export default usersSeed;
