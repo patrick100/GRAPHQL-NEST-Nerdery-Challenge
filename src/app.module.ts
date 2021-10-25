d60cea9f283bc86661c6384048b5a56e9dd475ab
Patrick Lazo <patrickdz96@gmail.com>
2021-10-25 14:44:28 -0500

    change yarn to npm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { User2Controller } from './user2/user2.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UsersController, User2Controller],
   providers: [AppService],
 })
 export class AppModule {}
