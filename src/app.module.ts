8f0e791a6bca26dfbd1b6109a68bccbc1a58969f
Patrick Lazo <patrickdz96@gmail.com>
2021-10-28 15:16:25 -0500

    feat: add like module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { LikesModule } from './likes/likes.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ProductsModule } from './products/products.module';
     AuthModule,
     CategoriesModule,
     ProductsModule,
+    LikesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
