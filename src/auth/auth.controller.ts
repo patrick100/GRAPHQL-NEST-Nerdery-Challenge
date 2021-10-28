7a7f48ed07aa13241d0f0b621c48ea83a58871dd
Alexander <alexander.pinto@ucsp.edu.pe>
2021-10-27 20:44:27 -0500

    add sign-up sign-in

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,8 @@ import {
   Get,
   Request,
   UseGuards,
+  UseInterceptors,
+  ClassSerializerInterceptor,
 } from '@nestjs/common';
 import { CreateUserDto } from 'src/users/dto/request/create-user.dto';
 import { UsersService } from 'src/users/users.service';
@@ -17,6 +19,7 @@ import { plainToClass } from 'class-transformer';
 import { SignInDto } from './dto/request/sign-in.dto';
 import { SignInDto } from './dto/request/sign-in.dto';
 
+@UseInterceptors(ClassSerializerInterceptor)
 @Controller()
 export class AuthController {
   constructor(
