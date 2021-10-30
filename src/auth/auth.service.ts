3184ae9deeb2a6a1443c4a59c585255005ae52af
Patrick Lazo <patrickdz96@gmail.com>
2021-10-29 23:52:13 -0500

    fix: interfaces names

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,7 +5,7 @@ import * as crypto from 'crypto';
 import { PrismaService } from 'prisma/prisma.service';
 import { UsersService } from 'src/users/users.service';
 import { SignInDto } from './dto/request/sign-in.dto';
-import AuthData from '../interfaces/authData.interface';
+import AuthData from '../interfaces/auth-data.interface';
 import { Token, User } from '@prisma/client';
 import { CreateUserDto } from 'src/users/dto/request/create-user.dto';
 import { sendEmail } from '../utils/email';
